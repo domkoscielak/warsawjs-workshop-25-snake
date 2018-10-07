5b7018c57614427a09c3bcd4ceb2fc59ed72b0c2
Dominik Koscielak <domkoscielak@gmail.com>
2018-10-07 17:26:39 +0200

    Updates

---
+++ precache-manifest.9a517e15401dd9a8ebaaed321e17f244.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/warsawjs-workshop-25-snake/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "9353bc6dd6b4e508ae29",
+    "url": "/warsawjs-workshop-25-snake/static/js/runtime~main.9353bc6d.js"
+  },
+  {
+    "revision": "2d881d8bd3988925f5ef",
+    "url": "/warsawjs-workshop-25-snake/static/js/main.2d881d8b.chunk.js"
+  },
+  {
+    "revision": "68c82d53f15ee7f84fc4",
+    "url": "/warsawjs-workshop-25-snake/static/js/1.68c82d53.chunk.js"
+  },
+  {
+    "revision": "2d881d8bd3988925f5ef",
+    "url": "/warsawjs-workshop-25-snake/static/css/main.8edba9d2.chunk.css"
+  },
+  {
+    "revision": "e18b3f0841094c4495e5a7915e28719c",
+    "url": "/warsawjs-workshop-25-snake/index.html"
+  }
+];
